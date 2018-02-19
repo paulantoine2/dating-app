16f7cf2d7cbf92d9453a9d4af0893f1517e9c69f
Paul <paul.antoine2@gmail.com>
2018-02-19 23:52:39 +0100

    Cards

--- src/app/pages/browse/browse.component.ts
+++ src/app/pages/browse/browse.component.ts
@@ -6,35 +6,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./browse.component.css']
 })
 export class BrowseComponent implements OnInit {
+  cards = ['Laura', 'Julie', 'Mathilde', 'Tina', 'Caro'];
 
   constructor() {
 
   }
 
-  ngOnInit() {
+  initSwipe() {
     const initPos = {
       x: 0,
       y: 0
     };
-    const card = document.getElementById('card');
+    const diff = {
+      x: 0,
+      y: 0
+    };
+    const card = <HTMLElement>document.getElementsByClassName('card')[0];
     card.addEventListener('touchstart', (ev) => {
       initPos.x = ev.touches[0].clientX;
       initPos.y = ev.touches[0].clientY;
     });
     card.addEventListener('touchmove', (ev) => {
-      const diff = {
-        x: ev.touches[0].clientX - initPos.x,
-        y: ev.touches[0].clientX - initPos.y,
-      };
+      diff.x = ev.touches[0].clientX - initPos.x;
+      diff.y = ev.touches[0].clientY - initPos.y;
       card.style.transform = `translateX(${diff.x}px)`;
     });
     card.addEventListener('touchend', (ev) => {
       card.style.transition = `all 0.2s ease`;
-      setTimeout(() => {
-        card.style.transition = null;
-      }, 200);
-      card.style.transform = `translateX(0px)`;
+      if (diff.x > 200) {
+        card.style.transform = `translateX(150%)`;
+        setTimeout(() => {
+          this.cards.shift();
+          setTimeout(() => {
+            this.initSwipe();
+          }, 100);
+        }, 200);
+      } else {
+        card.style.transform = `translateX(0px)`;
+        setTimeout(() => {
+          card.style.transition = null;
+        }, 200);
+      }
     });
+  }
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.initSwipe();
+    }, 100);
+
   }
 
 }
